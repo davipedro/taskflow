c5e7d97a114f8defd5b20ea5e06a785ac4e2b5bf
Davi Silva <davi.silva@gafit.com.br>
2025-12-02 18:28:34 -0300

    feat: add authorization traits to base controller and define project routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
@@ -13,5 +14,15 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'verified'])->prefix('projects')->group(function () {
+    Route::get('/', [ProjectController::class, 'index'])->name('projects.index');
+    Route::get('create', [ProjectController::class, 'create'])->name('projects.create');
+    Route::post('/', [ProjectController::class, 'store'])->name('projects.store');
+    Route::get('{project}', [ProjectController::class, 'show'])->name('projects.show');
+    Route::get('{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+    Route::patch('{project}', [ProjectController::class, 'update'])->name('projects.update');
+    Route::delete('{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+});
 
 require __DIR__.'/settings.php';
