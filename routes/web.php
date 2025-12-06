df8339402078a244276467ed64a52118d9435f76
Davi Silva <davi.silva@gafit.com.br>
2025-12-06 01:52:15 -0300

    feat: add task management routes
    - Introduced routes for creating and managing tasks associated with projects.
    - Added routes for task creation, viewing, editing, updating status, and priority.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
@@ -23,6 +24,19 @@ Route::middleware(['auth', 'verified'])->prefix('projects')->group(function () {
     Route::get('{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
     Route::patch('{project}', [ProjectController::class, 'update'])->name('projects.update');
     Route::delete('{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+
+    Route::get('{project}/tasks/create', [TaskController::class, 'createByProject'])->name('projects.tasks.create');
+    Route::post('{project}/tasks', [TaskController::class, 'store'])->name('projects.tasks.store');
+});
+
+Route::middleware(['auth', 'verified'])->prefix('tasks')->group(function () {
+    Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+    Route::get('{task}', [TaskController::class, 'show'])->name('tasks.show');
+    Route::get('{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+    Route::put('{task}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::patch('{task}/status', [TaskController::class, 'updateStatus'])->name('tasks.updateStatus');
+    Route::patch('{task}/priority', [TaskController::class, 'updatePriority'])->name('tasks.updatePriority');
+    Route::delete('{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
 });
 
 require __DIR__.'/settings.php';
